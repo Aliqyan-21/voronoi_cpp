d3ddafb29d60f5e59f77a3e9a4357858c56631b9
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-08-30 19:54:16 +0530

    fill_circle, random_seeds and markers

--- main.cpp
+++ main.cpp
@@ -4,7 +4,19 @@
 
 const int Width = 800, Height = 600;
 
-static uint32_t image[Height][Width];
+const int Seeds_count = 10;
+
+uint32_t image[Height][Width];
+
+#define BACKGROUND_COLOR 0xFF121212
+#define SEED_MARKER_RADIUS 5
+#define SEED_MARKER_COLOR 0xFFFFFFFF
+
+struct Point {
+  int x, y;
+};
+
+Point seeds[Seeds_count];
 
 void fill_image(uint32_t color) {
   for (int i = 0; i < Height; i++) {
@@ -40,9 +52,43 @@ void save_image_ppm(const std::string &filePath) {
   f.close();
 }
 
+void fill_cicle(int cx, int cy, int radius, uint32_t color) {
+  int x0 = cx - radius;
+  int y0 = cy - radius;
+  int x1 = cx + radius;
+  int y1 = cy + radius;
+
+  for (int i = x0; i <= x1; i++) {
+    if (i >= 0 && i < Width) {
+      for (int j = y0; j <= y1; j++) {
+        if (j >= 0 && j < Height) {
+          int dx = cx - i;
+          int dy = cy - j;
+
+          if (dx * dx + dy * dy <= radius * radius)
+            image[j][i] = color;
+        }
+      }
+    }
+  }
+}
+
+void generate_random_seeds() {
+  for (int i = 0; i < Seeds_count; i++) {
+    seeds[i].x = rand() % Width;
+    seeds[i].y = rand() % Height;
+  }
+}
+
 int main(void) {
   // aa bb gg rr --> 0xFF0000FF --> color_red
-  fill_image(0xFF0000FF);
+  fill_image(BACKGROUND_COLOR);
+
+  generate_random_seeds();
+
+  for (int i = 0; i < Seeds_count; i++) {
+    fill_cicle(seeds[i].x, seeds[i].y, SEED_MARKER_RADIUS, SEED_MARKER_COLOR);
+  }
 
   save_image_ppm("output.ppm");
 
